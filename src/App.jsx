b7824feb5ca8e5b2c55dfd7c0eabc1ee7f4cf158
zachleach@wsl <85465371+zachleach@users.noreply.github.com>
2024-12-10 19:44:27 -0600

    card hand without previews

---
+++ src/App.jsx
@@ -0,0 +1,79 @@
+import './App.css'
+
+
+const container_style = {
+	height: '100%', 
+	display: 'flex', 
+	flexDirection: 'column' 
+}
+
+const top_style = {
+	flex: '4',
+	backgroundColor: '#f0f0f0'
+}
+
+const card_width = 200;
+
+const bot_style = {
+	flex: '1',
+	display: 'flex',
+	position: 'relative',
+	padding: '8px',
+	backgroundColor: '#e0e0e0',
+	justifyContent: 'center',
+	alignItems: 'flex-end',
+	overflow: 'hidden'
+}
+
+const card_style = (index, total_cards) => {
+  const position = index - (total_cards - 1) / 2;
+	const m = 4;
+  const rotation = position * m;
+	const lower_by = 0.8 * card_width;
+  const vertical_offset = Math.pow(position, 2) * m + lower_by; 
+  
+  return {
+    width: `${card_width}`,
+    height: `${card_width / 0.714}px`,
+    backgroundColor: 'white',
+    border: '1px solid black',
+    borderRadius: '8px',
+    transform: `rotate(${rotation}deg) translateY(${vertical_offset}px)`,
+    transformOrigin: 'bottom center',
+    position: 'absolute',
+    left: `calc(50% - ${card_width / 2}px + ${position * 120}px)`
+  };
+};
+
+const card_image_style = {
+	width: '100%',
+	height: '100%',
+	objectFit: 'cover',
+	borderRadius: 'inherit'
+}
+
+import card_image from './img_test.png'
+
+const App = () => {
+	const cards = [0, 1, 2, 3, 4, 4, 4, 4];
+
+  return (
+    <div style={container_style}>
+
+      <div style={top_style}>
+        Top Region
+      </div>
+
+      <div style={bot_style}>
+				{cards.map((_, index) => (
+          <div key={index} style={card_style(index, cards.length)}>
+            <img src={card_image} alt="card" style={card_image_style} />
+          </div>
+        ))}
+      </div>
+
+    </div>
+  );
+}
+
+export default App
