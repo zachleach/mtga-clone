feccb72cd722dec12ece7f6f47767622931cac95
zachleach@wsl <85465371+zachleach@users.noreply.github.com>
2024-12-14 20:11:03 -0600

    2024-12-14 20:11:03

--- test/src/App.jsx
+++ test/src/App.jsx
@@ -5,47 +5,55 @@ import './App.css'
 
 
 /**
- * blank card object for testing purposes
+ * not really sure about the inner-style thing, need to look into it later
  */
-const Card = ({ w, content }) => {
+const Card = ({ content }) => {
 	const s = {
-		width: `${w}px`,
-		aspectRatio: '0.714',
+		width: '100%',
+		height: '0',
+		paddingBottom: '140%', 
+		position: 'relative',
 		border: '1px solid black',
 		borderRadius: '12px',
 		backgroundColor: 'white',
+	}
 
+	const innerStyle = {
+		position: 'absolute',
+		top: '0',
+		left: '0',
+		width: '100%',
+		height: '100%',
 		display: 'flex',
-		flex: '0 1 auto',
 		justifyContent: 'center',
 		alignItems: 'center',
 	}
 
 	return (
 		<div style={s}>
-			{content}
+			<div style={innerStyle}>
+				{content}
+			</div>
 		</div>
 	)
 }
 
 
 
+/**
+ * don't know how, but it makes the cards appear centered with the correct aspect ratio 
+ */
 const Cards = ({ N, w }) => {
 	return (
-		<>
+		<div style={{ display: 'flex', height: '100%', width: '100%' }}>
 			{Array(N).fill(null).map((_, index) => (
-				<div>
+				<div style={{ flex: `0 1 ${100/N}%`, height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
 					<Card key={index} w={w} />
 				</div>
 			))}
-		</>
+		</div>
 	)
 }
-
-
-
-
-
 
 
 
@@ -56,12 +64,10 @@ const Cards = ({ N, w }) => {
 const Container = ({ children, w, h, vertical }) => {
 	const s = {
 		display: 'flex',
-		width: w ? `${w}%` : '100%',
+		width: `${w}%`, 
 		height: `${h}%`,
 
 		boxSizing: 'border-box',
-
-		flexDirection: vertical ? 'column' : 'row',
 
 		backgroundColor: 'grey',
 		border: '1px solid red',
@@ -78,13 +84,16 @@ const Container = ({ children, w, h, vertical }) => {
 }
 
 
+
+
 const Board = () => {
 
 	return (
 		<div style={{ height: '100%', width: '100%', display: 'flex', flexDirection: 'column' }}>
 
+			{/* creature zone */}
 			<Container w={100} h={50}>
-				Creatures
+				<Cards N={10} w={90}/>
 			</Container>
 
 
@@ -118,9 +127,9 @@ const App = () => {
 	return (
 		<div>
 			<div style={{ height: '30vh', display: 'flex', transform: 'rotate(180deg)' }}>
-					<Board/>
-					<Board/>
-					<Board/>
+				<Board/>
+				<Board/>
+				<Board/>
 			</div>
 
 			<div style={{ height: '70vh', display: 'flex', flexDirection: 'column' }}>
