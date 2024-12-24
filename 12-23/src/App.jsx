7a810a1a530afc9e15ec8bd7f083e1801b2b1615
zachleach@wsl <85465371+zachleach@users.noreply.github.com>
2024-12-24 06:19:44 -0600

    2024-12-24 06:19:44

--- 12-23/src/App.jsx
+++ 12-23/src/App.jsx
@@ -4,6 +4,7 @@ import { OpponentBoard, PlayerBoard } from './components'
 const App = () => {
   return (
     <div style={{ height: '100vh', display: 'flex', flexDirection: 'column' }}>
+
       {/* Opponents Section - 30% height */}
       <div style={{ display: 'flex', flex: '0 0 30%', flexDirection: 'column', border: '1pt solid yellow', overflow: 'hidden' }}>
         <div style={{ display: 'flex', flex: '0 0 100%', flexDirection: 'row', overflow: 'hidden' }}>
@@ -17,6 +18,7 @@ const App = () => {
       <div style={{ display: 'flex', flex: '0 0 70%', overflow: 'hidden' }}>
         <PlayerBoard />
       </div>
+
     </div>
   )
 }
