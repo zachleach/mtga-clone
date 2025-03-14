263c1e767a5ff0e95e6712c72d57860756e0c99a
zachleach@wsl <85465371+zachleach@users.noreply.github.com>
2025-03-14 08:43:09 -0500

    remove claude from git history

--- claude/index.js
+++
@@ -1,8 +0,0 @@
-export { ScryfallUtil } from './ScryfallUtil'
-export { CardGridOverlay } from './CardGridOverlay'
-export { Card } from './Card'
-export { OpponentBoard } from './OpponentBoard'
-export { PlayerBoard } from './PlayerBoard'
-export { Row } from './Row'
-export { Stack } from './Stack'
-export { Server, ServerProvider } from './Server'
